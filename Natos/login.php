7efbc88e9fe35e97e9970250393dae61899efa2a
Carlos Cendejas <61759875+cbchar@users.noreply.github.com>
2020-03-11 15:10:08 -0600

    modificaciones
    Alta de formularios para pagina web.

---
+++ Natos/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> LO G I N </title>
+</head>
+<body>
+    <form action="" method="post">
+    <div><center>
+    <h2>Iniciar sesión</h2>
+    <input type="textbox" placeholder="username"><br>
+    <br><input type="password" placeholder="password"><br>
+    <br><input type="submit" values="Enviar">
+    </center>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Natos/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> M E N U </title>
+</head>
+<body>
+    <form action="" method="post">
+    <div><center>
+    <button>Empleados</button><br>
+    <br><button>Clientes</button><br>
+    <br><button>Proveedores</button><br>
+    <br><button>Compra</button><br>
+    <br><button>Venta</button><br>
+    <br><button>Inventario materia prima</button><br>
+    <br><button>Inventario tarimas</button><br>
+    </center></div>
+    </form>
+</body>
+</html>
